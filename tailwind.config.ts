bfa332b7b15c1eb0b7ff73d4660478c370fb2f37
NLS Eternament <nls.etenament@gmail.com>
2025-10-03 22:59:23 +0000

    Osea parece como q no tiene parte visual

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,3 @@
-import type {Config} from 'tailwindcss';
-
 export default {
   darkMode: ['class'],
   content: [
@@ -102,4 +100,4 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
